6de9dc8fa151eb0722711698aa55757400c1d84a
Yi donghoon <icq4ever@gmail.com>
2021-10-01 15:09:23 +0900

    add timestamp & OLED text display

--- restful2.py
+++ restful2.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+from daemonize import Daemonize
 import time
 import busio
 from digitalio import DigitalInOut, Direction, Pull
@@ -9,12 +11,17 @@ import adafruit_rfm9x
 from flask import Flask, request, jsonify
 #from multiprocessing import Process, Value
 from threading import Thread
+import datetime
+
+# Flask init
 app = Flask(__name__)
 
+#pid = "/tmp/restful.pid"
+
+# SET RFM9x RADIO FREQ
 RADIO_FREQ_MHZ = 900.0
-
+# init OLED with I2C
 i2c = busio.I2C(board.SCL, board.SDA)
-
 #LED Display
 display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x3c)
 # Clear the display.
@@ -42,6 +49,7 @@ def pkt_int_to_float(pkt_val_1, pkt_val_2, pkt_val_3 = None):
 def get_packet():
 	global temp_val
 	global humid_val
+	global timestamp_str
 	while True:
 		packet = None
 	
@@ -51,8 +59,9 @@ def get_packet():
 		#check for packet rx
 		packet = rfm9x.receive()
 		if packet is None :
+			display.fill(0)
 			display.show()
-			display.text('- Waiting for PKT -', 10, 20, 1)
+#			display.text('- Waiting for PKT -', 10, 20, 1)
 			time.sleep(1);
 		else:
 			prev_packet = packet
@@ -61,14 +70,19 @@ def get_packet():
 			#Decode packet
 			temp_val = pkt_int_to_float(packet[1], packet[2])
 			humid_val = pkt_int_to_float(packet[3], packet[4])
-	
+
+			# timestamp
+			now = datetime.datetime.now()	# current date and time
+			timestamp_str = now.strftime("%Y/%m/%d")+"-"+now.strftime("%H:%M:%S")		
+
 			#print packet information
 			print("Temp  : %0.2f C" % temp_val)
 			print("Humid : %0.2f %% " % humid_val)
-	
+
 			display.fill(0)
 			display.text('Weather Receiver', 0, 0, 1)
-			display.text('> ' + str(temp_val)+ "C / "+str(humid_val)+"%", 10, 20, 1);
+			display.text('> ' + str(temp_val)+ "C / "+str(humid_val)+"%", 0, 10, 1);
+			display.text("> " + timestamp_str, 0, 20, 1);
 			display.show()
 			time.sleep(1)
 		display.show()
@@ -82,11 +96,15 @@ def index():
 
 @app.route("/weather", methods = ['GET'])
 def return_weather_info():
-	return jsonify({"temperature" : temp_val, "humidity" : humid_val})
+	return jsonify({"temperature" : temp_val, "humidity" : humid_val, "timestamp" : timestamp_str})
 
-if __name__ == "__main__":
+def main():
 #	loop_on = Value('b', True)
+
 	p = Thread(target=get_packet, args=( ))
 	p.start()
 	app.run(host='0.0.0.0', debug=True, use_reloader=False, port=3005)
 	p.join()
+
+if __name__ == "__main__":
+    main()
